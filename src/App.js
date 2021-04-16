d502fb826d71f4ab49e2e3fccd69125cbffbcc15
user <user@WIN-T8OT79O9OG5>
2021-04-16 07:22:18 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+    <Navigation>Navigation</Navigation>
+    <Header>Header</Header>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+
+height: 100vh;
+
+display: grid;
+
+grid-template:
+"nav head" min-content
+"nav main" 1fr
+    / min-content 1fr;
+`;
+
+const Navigation = styled.div`
+grid-area: nav;
+
+overflow: auto;
+    resize: horizontal;
+    
+    min-width: 120px;
+    max-width: 50vw;
+`;
+
+const Header = styled.div`
+grid-area: head;
+overflow: auto;
+    resize: vertical;
+    
+    min-height: min-content;
+    max-height: 200px;
+`;
